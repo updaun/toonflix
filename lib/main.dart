b8d5e345b4823ddf5e64058d7de535d4bb8918bd
updaun <updauney@daum.net>
2024-07-19 04:38:26 +0900

    chore: header design start

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,41 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello flutter!'),
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hey Selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'Welcome back',
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
         ),
-        body: const Center(child: Text('Hello World!')),
       ),
     );
   }
